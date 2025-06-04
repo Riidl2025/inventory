647deb55b55a75a02e9ce28b0cebb214f8fdec9a
riidl2025 <avinash.dp@somaiya.edu>
2025-06-04 12:22:22 +0530

    new dashboard changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
 // config.js
-export const API_URL = "http://localhost:5000";
-// export const API_URL = "https://inventorybackend-1.onrender.com";
+// export const API_URL = "http://localhost:5000";
+export const API_URL = "https://inventorybackend-1.onrender.com";
 console.log("Using API URL:", API_URL)
